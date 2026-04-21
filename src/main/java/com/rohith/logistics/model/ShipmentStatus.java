be06a2cf27280dc691b96db9f682ec92822b8f3e
rohith-max003 <rohithlakkavarapu3@gmail.com>
2026-04-21 15:28:25 -0500

    Add src/main/java/com/rohith/logistics/model/ShipmentStatus.java

---
+++ src/main/java/com/rohith/logistics/model/ShipmentStatus.java
@@ -0,0 +1,11 @@
+package com.rohith.logistics.model;
+
+public enum ShipmentStatus {
+    CREATED,
+    PICKED_UP,
+    IN_TRANSIT,
+    OUT_FOR_DELIVERY,
+    DELIVERED,
+    EXCEPTION,
+    CANCELLED
+}
